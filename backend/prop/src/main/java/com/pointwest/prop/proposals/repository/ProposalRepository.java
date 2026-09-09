5f04e33e2e6f960fe4274c70b3db35fa8b5a7945
Romille Ilaida <romille.ilaida@pointwest.com>
2026-09-09 15:31:54 +0800

    [PRP-C2] feat(service-layer): create service layer for proposal package

---
+++ backend/prop/src/main/java/com/pointwest/prop/proposals/repository/ProposalRepository.java
@@ -0,0 +1,11 @@
+package com.pointwest.prop.proposals.repository;
+
+
+import com.pointwest.prop.common.entity.Proposal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProposalRepository extends JpaRepository<Proposal, Long>, JpaSpecificationExecutor<Proposal> {
+}
